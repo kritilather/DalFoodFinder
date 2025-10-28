e2d9bc01737f6160b137e611672030b23f600573
Lathers <123626748+klather@users.noreply.github.com>
2025-10-28 09:11:53 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="flex flex-rows w-1/2">
+  <div class="w-14 flex-none ...">01</div>
+  <div class="w-64 flex-initial ...">02</div>
+  <div class="w-32 flex-initial ...">03</div>
+</div>
+  )
+}
+
+export default App
